7e9fc32d8de16ac67bdeefecabb4f814e1e2bad1
Sam-I-am-BD <samu3lramb3rg@gmail.com>
2024-09-24 17:18:33 -0400

    Update commaCode.py
    Wrote some comments into the code.

--- commaCode.py
+++ commaCode.py
@@ -1,14 +1,14 @@
 # Comma Code
 spam = ['apples', 'bananas', 'tofu', 'cats', 'dogs']
 def commaCode(someList):
-    try:
-        someString = ''
-        for i in range(len(someList) - 1):
-            someString += str(someList[i]) + ', '
-        someString = someString + 'and ' + str(someList[-1])
+    try: # Added some error handling.
+        someString = '' 
+        for i in range(len(someList) - 1): # Creates a loop that iterates for however long the list is.
+            someString += str(someList[i]) + ', ' # adds a list item with a comma and a space.
+        someString = someString + 'and ' + str(someList[-1]) # adds a space and an "and" before the last item.
         print(someString)
-    except IndexError:
-        return print('The list is empty')
+    except IndexError: 
+        return print('The list is empty') # Added an exception
 commaCode(spam)
 
 
